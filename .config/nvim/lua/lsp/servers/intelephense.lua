e086bcb5337ded37884e3220282e344b915af6a2
Behzad Ehsani <b.ehsani@digikala.com>
2022-11-18 19:26:13 +0330

    fix root dir'

--- .config/nvim/lua/lsp/servers/intelephense.lua
+++ .config/nvim/lua/lsp/servers/intelephense.lua
@@ -1,4 +1,6 @@
+local file_dir = ParentDir(vim.api.nvim_buf_get_name(0), 1)
+local git_root = vim.fn.system("cd ".. file_dir .. " && " .."git rev-parse --show-toplevel")
 return {
-    root_dir = function() return ParentDir(vim.fn.getcwd(), 2) end,
+    root_dir = function() return ParentDir(git_root, 2) end,
 }
 
