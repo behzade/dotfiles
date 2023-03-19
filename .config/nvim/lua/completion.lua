c544f4c32a4d0acc716a3e951ae1d5b4b75e098d
Behzad Ehsani <ehsani.eof@gmail.com>
2023-03-19 11:58:27 +0330

    feat: add luasnip jump to node keybindings

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -53,3 +53,11 @@ local autopairs = require("nvim-autopairs")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 autopairs.setup({ check_ts = true })
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+
+
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
