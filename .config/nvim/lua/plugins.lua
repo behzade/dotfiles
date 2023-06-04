31f631fe3450ba5468f55022be3879ca344bd050
Behzad Ehsani <b.ehsani@digikala.com>
2023-06-04 13:52:32 +0330

    fix: build for fzf native

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -49,9 +49,7 @@ return require("lazy").setup({
         "nvim-telescope/telescope.nvim",
         dependencies = {
             {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build =
-                'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+                { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
             },
         },
         config = require("telescope-conf"),
