5345524baafbe81f7568a64e63abe924cd9fa49d
Behzad Ehsani <b.ehsani@digikala.com>
2023-06-05 15:25:58 +0330

    fix: remove print

--- .config/nvim/lua/lf.lua
+++ .config/nvim/lua/lf.lua
@@ -13,7 +13,6 @@ local function hijack_directory()
     if vim.fn.isdirectory(path) == 0 then
         return
     end
-    print(path)
     local bufnr = vim.fn.bufnr()
     vim.cmd(string.format('keepjumps keepalt term lf  %s', vim.fn.fnameescape(path)))
     vim.cmd(string.format('silent! bwipeout %d', bufnr))
