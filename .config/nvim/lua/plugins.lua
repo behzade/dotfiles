25cee6c9950bd728379567a22463f49700e1a642
Behzad Ehsani <b.ehsani@digikala.com>
2023-07-10 13:27:04 +0330

    feat: add debugger

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -171,5 +171,12 @@ return require("lazy").setup({
         event = { "CmdlineEnter" },
         ft = { "go", 'gomod' },
         build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text"
+        }
     }
 })
