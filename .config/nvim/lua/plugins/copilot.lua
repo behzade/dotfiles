a8946fe44a25a19d36462f335d5079503b07a98d
Behzad Ehsani <ehsani.eof@gmail.com>
2024-05-31 01:55:18 +0330

    add supermaven

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -13,4 +13,16 @@ return {
     --         })
     --     end,
     -- },
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({
+                keymaps = {
+                    accept_suggestion = "<Right>",
+                    clear_suggestion = "<C-]>",
+                    accept_word = "<C-j>",
+                },
+            })
+        end,
+    },
 }
